3a32179bbfcb2d291c62aa09bd64f5e2ca5e624e
Mathieu Besançon <mathieu.besancon@gmail.com>
2021-03-31 23:27:01 +0200

    adding generator

--- gen/generate_wrapper.jl
+++ gen/generate_wrapper.jl
@@ -1,10 +1,37 @@
 using Clang
 
-all_headers = readdir(joinpath(SOPLEX_DIR, "src", "soplex"))
-soplex_headers = vcat(
-    filter(h -> startswith(h, "type_"), all_headers),
-    filter(h -> startswith(h, "pub_"), all_headers),
-    filter(h -> startswith(h, "scip_"), all_headers),
-    "scipdefplugins.h",
-    filter(h -> startswith(h, "cons_"), all_headers),
-    "intervalarith.h",                                 # for nlpi/pub_expr
+const HEADER_BASE = "/usr/include"
+
+const SOPLEX_SRC = ENV["SOPLEX_SRC_DIR"]
+@assert ispath(SOPLEX_SRC)
+
+const SOPLEX_BUILD_DIR = if haskey(ENV, "SOPLEX_BUILD_DIR")
+    ENV["SOPLEX_BUILD_DIR"]
+else
+    p = joinpath(SOPLEX_SRC, "build")
+    if ispath(p)
+        p
+    else
+        p = joinpath(SOPLEX_SRC, "debug")
+    end
+    p
+end
+
+@assert ispath(SOPLEX_BUILD_DIR)
+
+soplex_header = joinpath(SOPLEX_SRC, "src", "soplex_interface.h")
+@assert isfile(soplex_header)
+
+context = Clang.init(
+    headers=[soplex_header],
+    common_file="commons.jl",
+    output_dir="../src/",
+    clang_includes=vcat(Clang.HEADER_BASE, Clang.LLVM_INCLUDE),
+    clang_args = ["-I", Clang.HEADER_BASE],
+    clang_diagnostics=true,
+    header_wrapped=(header, cursorname) -> header == cursorname,
+    header_library=header_name -> "libsoplex"
+)
+Clang.run(context)
+
+rm(joinpath(@__DIR__, "..", "src", "LibTemplate.jl"))
---
+++ src/soplex_interface.jl
@@ -0,0 +1,99 @@
+# Julia wrapper for header: soplex_interface.h
+# Automatically generated using Clang.jl
+
+
+function SoPlex_create()
+    ccall((:SoPlex_create, libsoplex), Ptr{Cvoid}, ())
+end
+
+function SoPlex_free(soplex)
+    ccall((:SoPlex_free, libsoplex), Cvoid, (Ptr{Cvoid},), soplex)
+end
+
+function SoPlex_numRows(soplex)
+    ccall((:SoPlex_numRows, libsoplex), Cint, (Ptr{Cvoid},), soplex)
+end
+
+function SoPlex_numCols(soplex)
+    ccall((:SoPlex_numCols, libsoplex), Cint, (Ptr{Cvoid},), soplex)
+end
+
+function SoPlex_setRational(soplex)
+    ccall((:SoPlex_setRational, libsoplex), Cvoid, (Ptr{Cvoid},), soplex)
+end
+
+function SoPlex_setIntParam(soplex, paramcode, paramvalue)
+    ccall((:SoPlex_setIntParam, libsoplex), Cvoid, (Ptr{Cvoid}, Cint, Cint), soplex, paramcode, paramvalue)
+end
+
+function SoPlex_addColReal(soplex, colentries, colsize, nnonzeros, objval, lb, ub)
+    ccall((:SoPlex_addColReal, libsoplex), Cvoid, (Ptr{Cvoid}, Ptr{Cdouble}, Cint, Cint, Cdouble, Cdouble, Cdouble), soplex, colentries, colsize, nnonzeros, objval, lb, ub)
+end
+
+function SoPlex_addColRational(soplex, colnums, coldenoms, colsize, nnonzeros, objvalnum, objvaldenom, lbnum, lbdenom, ubnum, ubdenom)
+    ccall((:SoPlex_addColRational, libsoplex), Cvoid, (Ptr{Cvoid}, Ptr{Cint}, Ptr{Cint}, Cint, Cint, Cint, Cint, Cint, Cint, Cint, Cint), soplex, colnums, coldenoms, colsize, nnonzeros, objvalnum, objvaldenom, lbnum, lbdenom, ubnum, ubdenom)
+end
+
+function SoPlex_addRowReal(soplex, rowentries, rowsize, nnonzeros, lb, ub)
+    ccall((:SoPlex_addRowReal, libsoplex), Cvoid, (Ptr{Cvoid}, Ptr{Cdouble}, Cint, Cint, Cdouble, Cdouble), soplex, rowentries, rowsize, nnonzeros, lb, ub)
+end
+
+function SoPlex_addRowRational(soplex, rownums, rowdenoms, rowsize, nnonzeros, lbnum, lbdenom, ubnum, ubdenom)
+    ccall((:SoPlex_addRowRational, libsoplex), Cvoid, (Ptr{Cvoid}, Ptr{Cint}, Ptr{Cint}, Cint, Cint, Cint, Cint, Cint, Cint), soplex, rownums, rowdenoms, rowsize, nnonzeros, lbnum, lbdenom, ubnum, ubdenom)
+end
+
+function SoPlex_getPrimalReal(soplex, primal, dim)
+    ccall((:SoPlex_getPrimalReal, libsoplex), Cvoid, (Ptr{Cvoid}, Ptr{Cdouble}, Cint), soplex, primal, dim)
+end
+
+function SoPlex_getPrimalRationalString(soplex, dim)
+    ccall((:SoPlex_getPrimalRationalString, libsoplex), Cstring, (Ptr{Cvoid}, Cint), soplex, dim)
+end
+
+function SoPlex_getDualReal(soplex, dual, dim)
+    ccall((:SoPlex_getDualReal, libsoplex), Cvoid, (Ptr{Cvoid}, Ptr{Cdouble}, Cint), soplex, dual, dim)
+end
+
+function SoPlex_optimize(soplex)
+    ccall((:SoPlex_optimize, libsoplex), Cint, (Ptr{Cvoid},), soplex)
+end
+
+function SoPlex_changeObjReal(soplex, obj, dim)
+    ccall((:SoPlex_changeObjReal, libsoplex), Cvoid, (Ptr{Cvoid}, Ptr{Cdouble}, Cint), soplex, obj, dim)
+end
+
+function SoPlex_changeObjRational(soplex, objnums, objdenoms, dim)
+    ccall((:SoPlex_changeObjRational, libsoplex), Cvoid, (Ptr{Cvoid}, Ptr{Cint}, Ptr{Cint}, Cint), soplex, objnums, objdenoms, dim)
+end
+
+function SoPlex_changeLhsReal(soplex, lhs, dim)
+    ccall((:SoPlex_changeLhsReal, libsoplex), Cvoid, (Ptr{Cvoid}, Ptr{Cdouble}, Cint), soplex, lhs, dim)
+end
+
+function SoPlex_changeLhsRational(soplex, lhsnums, lhsdenoms, dim)
+    ccall((:SoPlex_changeLhsRational, libsoplex), Cvoid, (Ptr{Cvoid}, Ptr{Cint}, Ptr{Cint}, Cint), soplex, lhsnums, lhsdenoms, dim)
+end
+
+function SoPlex_changeRhsReal(soplex, rhs, dim)
+    ccall((:SoPlex_changeRhsReal, libsoplex), Cvoid, (Ptr{Cvoid}, Ptr{Cdouble}, Cint), soplex, rhs, dim)
+end
+
+function SoPlex_changeRhsRational(soplex, rhsnums, rhsdenoms, dim)
+    ccall((:SoPlex_changeRhsRational, libsoplex), Cvoid, (Ptr{Cvoid}, Ptr{Cint}, Ptr{Cint}, Cint), soplex, rhsnums, rhsdenoms, dim)
+end
+
+function SoPlex_writeFileReal(soplex, filename)
+    ccall((:SoPlex_writeFileReal, libsoplex), Cvoid, (Ptr{Cvoid}, Cstring), soplex, filename)
+end
+
+function SoPlex_objValueReal(soplex)
+    ccall((:SoPlex_objValueReal, libsoplex), Cdouble, (Ptr{Cvoid},), soplex)
+end
+
+function SoPlex_objValueRationalString(soplex)
+    ccall((:SoPlex_objValueRationalString, libsoplex), Cstring, (Ptr{Cvoid},), soplex)
+end
+
+function SoPlex_changeBoundsReal(soplex, lb, ub, dim)
+    ccall((:SoPlex_changeBoundsReal, libsoplex), Cvoid, (Ptr{Cvoid}, Ptr{Cdouble}, Ptr{Cdouble}, Cint), soplex, lb, ub, dim)
+end
